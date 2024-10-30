373d0b235a951bfb282b1a5b70ce33c309cb0644
Thomas Harte <thomas.harte@gmail.com>
2024-10-30 11:42:22 -0400

    Add map file for Sim Coupé.

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-~/Downloads/pyz80-main/pyz80 -I binary/SAMDOS2 -o build/kidd.dsk main.z80s && open -a SimCoupe build/kidd.dsk
+~/Downloads/pyz80-main/pyz80 -I binary/SAMDOS2 --mapfile build/kidd.map -o build/kidd.dsk main.z80s && open -a SimCoupe build/kidd.dsk
