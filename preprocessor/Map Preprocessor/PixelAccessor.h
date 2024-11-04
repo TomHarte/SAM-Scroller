438e68b1ce18c6410b0ef7529b5c264887ca3a39
Thomas Harte <thomas.harte@gmail.com>
2024-11-04 17:27:09 -0500

    Add notes to self, begin thinking about the paletted world.

--- preprocessor/Map Preprocessor/PixelAccessor.h
+++ preprocessor/Map Preprocessor/PixelAccessor.h
@@ -7,6 +7,9 @@
 
 #pragma once
 
+#include <map>
+#include <vector>
+
 /*!
 	Wraps the necessary CGBitmap calls to take an NSImage and provide
 	access to its pixel contents.
@@ -36,11 +39,19 @@ class PixelAccessor {
 			CGColorSpaceRelease(colour_space_);
 		}
 
-		size_t bytes_per_row() { return bytes_per_row_; }
-		size_t width() { return width_; }
-		size_t height() { return height_; }
-		uint32_t *pixels(size_t x, size_t y) { return reinterpret_cast<uint32_t *>(&pixels_[y*bytes_per_row_ + x*4]); }
-		uint32_t pixel(size_t x, size_t y) { return *pixels(x, y); }
+		size_t bytes_per_row() const { return bytes_per_row_; }
+		size_t width() const { return width_; }
+		size_t height() const { return height_; }
+		uint32_t pixel(size_t x, size_t y) const { return *pixels(x, y); }
+
+		// The following isn't const correct because it's used by Objective-C
+		// to call into other C functions, which have no corresponding concept.
+		uint32_t *pixels(size_t x, size_t y) {
+			return reinterpret_cast<uint32_t *>(&pixels_[y*bytes_per_row_ + x*4]);
+		}
+		const uint32_t *pixels(size_t x, size_t y) const {
+			return reinterpret_cast<uint32_t *>(&pixels_[y*bytes_per_row_ + x*4]);
+		}
 
 	private:
 		size_t width_, height_;
@@ -48,4 +59,33 @@ class PixelAccessor {
 		CGColorSpaceRef colour_space_;
 		uint8_t *pixels_;
 		size_t bytes_per_row_;
+};
+
+/*!
+	Takes a PixelAccessor and an active palette, maps it through the paltte and subsequently
+	provides pixels as 4bpp palette entries.
+*/
+class PalettedPixelAccessor {
+	public:
+		PalettedPixelAccessor(const PixelAccessor &accessor, const std::map<uint32_t, uint8_t> &palette) :
+			width_(accessor.width()),
+			height_(accessor.height())
+		{
+			pixels_.resize(width_ * height_);
+			for(size_t y = 0; y < height_; y++) {
+				for(size_t x = 0; x < width_; x++) {
+					const uint32_t source_colour = accessor.pixel(x, y);
+					const auto iterator = palette.find(source_colour);
+					pixels_[y * width_ + x] = iterator != palette.end() ? iterator->second : 0;
+				}
+			}
+		}
+
+		size_t width() const { return width_; }
+		size_t height() const { return height_; }
+		uint32_t pixel(size_t x, size_t y) const { return pixels_[y * width_ + x]; }
+
+	private:
+		size_t width_, height_;
+		std::vector<uint8_t> pixels_;
 };
--- preprocessor/Map Preprocessor/SpriteSerialiser.h
+++ preprocessor/Map Preprocessor/SpriteSerialiser.h
@@ -17,11 +17,7 @@ class SpriteSerialiser {
 			int hl = 0;
 			for(size_t y = 0; y < accessor.height(); y ++) {
 				for(size_t x = 0; x < accessor.width(); x += 2) {
-					const uint32_t left = accessor.pixel(x, y);
-					const uint32_t right = accessor.pixel(x + 1, y);
-
-					// Skip transparent pixels.
-					if((left >> 24) == 0 && (right >> 24) == 0) {
+					if(!has_pixels_at(x, y)) {
 						continue;
 					}
 
@@ -36,24 +32,28 @@ class SpriteSerialiser {
 				}
 			}
 
+			// TODO: LD (HL), r is faster than LD (HL), n, so write and test
+			// some sort of register allocator here to find out what it can
+			// reduce the LD (HL) code to, for fair comparison.
+			//
+			// Conversely, LD (ix+d), n seems to cost 'the same' as LD (ix+d), r
+			// (modulo the Sam's limited memory availability) so possibly not as
+			// vital there.
+
 			// Evaluate cost of using IX as the output cursor.
 			int ix_cost = 16;	// Loading cost.
 			for(size_t y = 0; y < accessor.height(); y ++) {
 				if(y&1) ix_cost += 8;	// INC IXh
 
 				for(size_t x = 0; x < accessor.width(); x += 2) {
-					const uint32_t left = accessor.pixel(x, y);
-					const uint32_t right = accessor.pixel(x + 1, y);
-
-					// Skip transparent pixels.
-					if((left >> 24) == 0 && (right >> 24) == 0) {
+					if(!has_pixels_at(x, y)) {
 						continue;
 					}
 					ix_cost += 19;		// LD (IX+d), n
 				}
 			}
 
-			// Evaluate cost of using the stack pointer.
+			// TODO: evaluate cost of using the stack pointer.
 //			int sp_cost = 4 + 10 + 6;	// Loading cost.
 //			int sp = 0;
 //			for(size_t y = accessor.height() - 1; y >= 0; y --) {
@@ -73,12 +73,11 @@ class SpriteSerialiser {
 			IX, HL,
 		} strategy;
 
-//		std::optional<uint8_t> pixels_at(size_t x, size_t y) {
-//			const uint32_t left = accessor.pixel(x, y);
-//			const uint32_t right = accessor.pixel(x + 1, y);
-//
-//			if((left >> 24) == 0 && (right >> 24) == 0) return {};
-//			return
-//		}
+		bool has_pixels_at(size_t x, size_t y) {
+			const uint32_t left = accessor_.pixel(x, y);
+			const uint32_t right = accessor_.pixel(x + 1, y);
+
+			return (left >> 24) != 0 || (right >> 24) != 0;
+		}
 };
 
