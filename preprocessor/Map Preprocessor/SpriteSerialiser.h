97a23827e1f662a3bf3a3cf707c5ca990ccad2e1
Thomas Harte <thomas.harte@gmail.com>
2024-11-04 17:13:55 -0500

    Fix line counting, run up against wall: this should work in palette-mapped space.

--- preprocessor/Map Preprocessor/SpriteSerialiser.h
+++ preprocessor/Map Preprocessor/SpriteSerialiser.h
@@ -15,7 +15,7 @@ class SpriteSerialiser {
 			// Evaluate cost of using HL as the output cursor.
 			int hl_cost = 0;
 			int hl = 0;
-			for(size_t y = 0; y < accessor.height(); y += 2) {
+			for(size_t y = 0; y < accessor.height(); y ++) {
 				for(size_t x = 0; x < accessor.width(); x += 2) {
 					const uint32_t left = accessor.pixel(x, y);
 					const uint32_t right = accessor.pixel(x + 1, y);
@@ -38,7 +38,7 @@ class SpriteSerialiser {
 
 			// Evaluate cost of using IX as the output cursor.
 			int ix_cost = 16;	// Loading cost.
-			for(size_t y = 0; y < accessor.height(); y += 2) {
+			for(size_t y = 0; y < accessor.height(); y ++) {
 				if(y&1) ix_cost += 8;	// INC IXh
 
 				for(size_t x = 0; x < accessor.width(); x += 2) {
@@ -53,6 +53,16 @@ class SpriteSerialiser {
 				}
 			}
 
+			// Evaluate cost of using the stack pointer.
+//			int sp_cost = 4 + 10 + 6;	// Loading cost.
+//			int sp = 0;
+//			for(size_t y = accessor.height() - 1; y >= 0; y --) {
+//				for(size_t x = accessor.width(); x >= 0; x -= 2) {
+//					const uint32_t left = accessor.pixel(x, y);
+//					const uint32_t right = accessor.pixel(x + 1, y);
+//				}
+//			}
+
 			strategy = hl_cost < ix_cost ? OutputStrategy::HL : OutputStrategy::IX;
 		}
 
@@ -62,5 +72,13 @@ class SpriteSerialiser {
 		enum class OutputStrategy {
 			IX, HL,
 		} strategy;
+
+//		std::optional<uint8_t> pixels_at(size_t x, size_t y) {
+//			const uint32_t left = accessor.pixel(x, y);
+//			const uint32_t right = accessor.pixel(x + 1, y);
+//
+//			if((left >> 24) == 0 && (right >> 24) == 0) return {};
+//			return
+//		}
 };
 
