52455d184364be18c6796fbfb0c3b3d0db9bc6ec
Thomas Harte <thomas.harte@gmail.com>
2024-11-05 21:31:27 -0500

    Use A for something rather than nothing.

--- preprocessor/Map Preprocessor/RegisterAllocator.h
+++ preprocessor/Map Preprocessor/RegisterAllocator.h
@@ -75,14 +75,62 @@ class RegisterAllocator {
 					case TileEvent::Type::OutputWord:
 						++word_references_[event.content];
 					break;
-					case TileEvent::Type::OutputByte:
-						++byte_references_[event.content];
-					break;
 				}
 			}
 
-			// Reset state.
+			// Look for IY and A optimisations.
 			serialiser.reset();
+
+			// Reset state.
+			reset();
+			auto word_references = word_references_;
+			while(true) {
+				const auto next = serialiser.next();
+				if(next.type == TileEvent::Type::Stop) {
+					break;
+				}
+
+				switch(next.type) {
+					default: break;
+
+					case TileEvent::Type::OutputWord: {
+						// const auto event =
+						next_word(next.content);
+					} break;
+
+					case TileEvent::Type::OutputByte: {
+						const auto event = next_byte(next.content);
+						if(event.type == RegisterEvent::Type::UseConstant) {
+							++byte_references_[event.value];
+						}
+					} break;
+				}
+			}
+
+			// Restore original word references list.
+			word_references_ = word_references;
+
+			// A allocation strategy is as dense as can be: use the most-recurring
+			// value, as long as it's at least 3.
+			if(!byte_references_.empty()) {
+				auto greatest = byte_references_.begin();
+				auto current = greatest;
+				++current;
+				while(current != byte_references_.end()) {
+					if(current->second > greatest->second) {
+						greatest = current;
+					}
+					++current;
+				}
+				if(greatest->second > 2) {
+					a_ = greatest->first;
+				}
+			}
+
+			// Clear state.
+			is_preview_ = false;
+			serialiser.reset();
+			reset();
 		}
 
 		RegisterEvent next_word(uint16_t value) {
@@ -143,8 +191,33 @@ class RegisterAllocator {
 				return RegisterEvent{.type = RegisterEvent::Type::Reuse, .reg = RegisterEvent::Register::D};
 			}
 
-			// TODO: intelligent use of A?
-			return RegisterEvent{.type = RegisterEvent::Type::UseConstant, .value = value};
+//			if(!is_preview_) {
+//				const auto prior_refs = byte_references_[value];
+//				--byte_references_[value];
+//
+//				// Loading to a register only costs less if the value is reused
+//				// more than twice.
+//				if(prior_refs > 2) {
+//					if(!a_) {
+//						a_ = value;
+//						return RegisterEvent{.reg = RegisterEvent::Register::A, .type = RegisterEvent::Type::Load, .value = value};
+//					}
+//				}
+//			}
+
+			if(a_ && *a_ == value) {
+				bool is_load = !has_loaded_a_;
+				has_loaded_a_ = true;
+				return RegisterEvent{.reg = RegisterEvent::Register::A, .type = is_load ? RegisterEvent::Type::Load : RegisterEvent::Type::Reuse, .value = value};
+			} else {
+				return RegisterEvent{.type = RegisterEvent::Type::UseConstant, .value = value};
+			}
+		}
+
+		void reset() {
+			bc_ = {};
+			de_ = {};
+			a_ = {};
 		}
 
 	private:
@@ -154,4 +227,7 @@ class RegisterAllocator {
 		std::optional<uint16_t> bc_;
 		std::optional<uint16_t> de_;
 		std::optional<uint8_t> a_;
+
+		bool is_preview_ = true;
+		bool has_loaded_a_ = false;
 };
