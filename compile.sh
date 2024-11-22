48c55217e104628167b20c4434a5fcd03b58d881
Thomas Harte <thomas.harte@gmail.com>
2024-11-21 21:14:53 -0500

    Redisable border profiling.

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-~/Downloads/pyz80-main/pyz80 -D BORDER_PROFILING=1 -I binary/SAMDOS2 --mapfile build/kidd.map -o build/kidd.dsk src/main.z80s && open -a SimCoupe build/kidd.dsk
+~/Downloads/pyz80-main/pyz80 -D BORDER_PROFILING=0 -I binary/SAMDOS2 --mapfile build/kidd.map -o build/kidd.dsk src/main.z80s && open -a SimCoupe build/kidd.dsk
